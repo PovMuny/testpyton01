b705e46f71584066510f3def0daa5159bbde2244
PovMuny <konpovlove1997@gmail.com>
2024-07-20 10:03:31 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+print("hello monk");print("muny");
+print("single 'quote' inside double quote")
+print('double quote inside "test" single quote ')
+
+print("single \'quote\' inside \"test quote\" double quote")
+
+print("WELLCOME AMOUNT CACULATE")
+
+qty1 = int(input("Enter the quantity1: "))
+qty2 = int(input("Enter the quantity2: "))
+price = float(input("Enter the price: "))
+qty=qty1+qty2
+print("Total Qty= " +str(qty))
+total_amount = qty * price
+print("The total amount is:", total_amount)
